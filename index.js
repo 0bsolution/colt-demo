49cff2b13764f86d0baa79f9ce13a09180c2a6af
chndt <chris@97ck.de>
2024-05-10 15:23:14 +0200

    Automated updates based on review comments

--- index.js
+++ index.js
@@ -6,23 +6,18 @@ const PORT = 3000;
 app.get('/convert-to-usd/:currency', async (req, res) => {
   const { currency } = req.params;
   try {
-    axios.get('https://api.exchangerate-api.com/v4/latest/USD')
-      .then(response => {
-        const rate = response.data.rates[currency.toUpperCase()];
-        if (!rate) {
-          return res.status(404).send('Currency not supported.');
-        }
-        const responseObj = {
-          convertedAmount: rate,
-          currency: 'USD'
-        };
-        res.json(responseObj);
-      })
-      .catch(error => {
-        console.error('Error fetching exchange rates:', error);
-        res.status(500).send('Error fetching currency data.');
-      });
+    const response = await axios.get('https://api.exchangerate-api.com/v4/latest/USD');
+    const rate = response.data.rates[currency.toUpperCase()];
+    if (!rate) {
+      return res.status(404).send('Currency not supported.');
+    }
+    const responseObj = {
+      convertedAmount: rate,
+      currency: 'USD'
+    };
+    res.json(responseObj);
   } catch (error) {
+    console.error('Error fetching exchange rates:', error);
     res.status(500).send('Error fetching currency data.');
   }
 });
@@ -31,24 +26,18 @@ app.get('/crypto-to-usd/:currency', async (req, res) => {
   const { currency } = req.params;
   try {
     const url = `https://api.coingecko.com/api/v3/simple/price?ids=${currency}&vs_currencies=usd`;
-    axios.get(url)
-      .then(response => {
-        if (!response.data[currency]) {
-          return res.status(404).send('Currency not supported.');
-        }
-        const responseObj = {
-          convertedAmount: response.data[currency].usd,
-          currency: 'USD'
-        };
-        res.json(responseObj);
-      })
-      .catch(error => {
-        console.error('Error fetching crypto data:', error);
-        res.status(500).send('Error fetching crypto data.');
-      });
+    const response = await axios.get(url);
+    if (!response.data[currency]) {
+      return res.status(404).send('Currency not supported.');
+    }
+    const responseObj = {
+      convertedAmount: response.data[currency].usd,
+      currency: 'USD'
+    };
+    res.json(responseObj);
   } catch (error) {
-    console.error('Error in crypto-to-usd route:', error);
-    res.status(500).send('Server error.');
+    console.error('Error fetching crypto data:', error);
+    res.status(500).send('Error fetching crypto data.');
   }
 });
 
